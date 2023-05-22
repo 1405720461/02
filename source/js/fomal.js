9add013116dec5e84292811ee074714ae10002a7
wjl <1405720461@qq.com>
2023-05-22 21:21:30 +0800

    更改小猫样式

--- source/js/fomal.js
+++ source/js/fomal.js
@@ -404,12 +404,12 @@ if (document.body.clientWidth > 992) {
     $.fn.nekoScroll = function (option) {
       var defaultSetting = {
         top: '0',
-        scroWidth: 6 + 'px',
+        scroWidth: 3 + 'px',
         z_index: 9999,
         zoom: 0.9,
         borderRadius: 5 + 'px',
         right: 55.6 + 'px',
-        nekoImg: "https://bu.dusays.com/2022/07/20/62d812db74be9.png",
+        nekoImg: "/assets/111.png",
         hoverMsg: "春天啦~",
         color: "var(--theme-color)",
         during: 500,
@@ -497,27 +497,22 @@ if (document.body.clientWidth > 992) {
     $("#myscoll").nekoScroll({
       bgcolor: 'rgb(0 0 0 / .5)', //背景颜色，没有绳子背景图片时有效
       borderRadius: '2em',
-      zoom: 0.9
+      zoom: 0.9,
+      hoverMsg:'', //鼠标浮动到neko上方的对话框信息
+      // nekoname:'neko1', //nekoname，相当于id
+      // nekoImg:'img/猫咪.png', //neko的背景图片
+      // scImg:"img/绳1.png", //绳子的背景图片
+      // bgcolor:'#1e90ff', //背景颜色，没有绳子背景图片时有效
+      // zoom:0.9, //绳子长度的缩放值
+      // right:'100px', //距离页面右边的距离
+      // fontFamily:'楷体', //对话框字体
+      // fontSize:'14px', //对话框字体的大小
+      // color:'#1e90ff', //对话框字体颜色
+      // scroWidth:'3px', //绳子的宽度
+      // z_index:100, //不用解释了吧
+      // during:1200, //从顶部到底部滑动的时长
     }
     );
-    //自定义（去掉以下注释，并注释掉其他的查看效果）
-    /*
-    $("#myscoll").nekoScroll({
-        nekoname:'neko1', //nekoname，相当于id
-        nekoImg:'img/猫咪.png', //neko的背景图片
-        scImg:"img/绳1.png", //绳子的背景图片
-        bgcolor:'#1e90ff', //背景颜色，没有绳子背景图片时有效
-        zoom:0.9, //绳子长度的缩放值
-        hoverMsg:'你好~喵', //鼠标浮动到neko上方的对话框信息
-        right:'100px', //距离页面右边的距离
-        fontFamily:'楷体', //对话框字体
-        fontSize:'14px', //对话框字体的大小
-        color:'#1e90ff', //对话框字体颜色
-        scroWidth:'8px', //绳子的宽度
-        z_index:100, //不用解释了吧
-        during:1200, //从顶部到底部滑动的时长
-    });
-    */
   })
 }
 
