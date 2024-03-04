ac07f889e924a5c332e79d4a80aef8b4aafdef4b
wjl <1405720461@qq.com>
2024-03-04 21:19:23 +0800

    关闭雪花开关

--- source/js/fomal.js
+++ source/js/fomal.js
@@ -2678,7 +2678,7 @@ function setUniverse() {
 
 // 雪花开关
 if (localStorage.getItem("snow") == undefined) {
-  localStorage.setItem("snow", "block");
+  localStorage.setItem("snow", "none");
 }
 document.getElementById("snow").style.display = localStorage.getItem("snow");
 function setSnow() {
