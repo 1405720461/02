a663183eb7cf9a5402ca2d999af2c6207a71e003
wjl <1405720461@qq.com>
2024-04-18 10:34:56 +0800

    ✨ feat(兔兔语录): 语录分块，修改随机函数

--- source/js/wisdom.js
+++ source/js/wisdom.js
@@ -1,5 +1,4 @@
-const quotes = [
-    "爱你，老妈",
+const SimpleWisdomQuotes = [
     "少年拥有了时间，时间带走了年少",
     "吃什么补什么，吃苦不能成为人上人，吃人才能",
     "暴雨中前进，伞是倒划天空的船",
@@ -9,7 +8,27 @@ const quotes = [
     "我最新的照片，其实是我最老的照片",
 ];
 
+const LOLQuotes = [
+    "爱你，老妈，明天见",
+    "如果真相带来痛苦，谎言只会雪上加霜",
+    "谎言不会伤人，真相才是快刀",
+    "世界既不黑也不白，而是一道精致的灰",
+    "我曾踏足山巅，也曾进入低谷，二者都让我受益良多",
+    "真正的大师，永远都怀着一颗学徒的心",
+]
+
+const quotesCollection = {
+    SimpleWisdomQuotes,
+    LOLQuotes
+}
+
 function getRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    return quotes[randomIndex];
+    const collectionKeys = Object.keys(quotesCollection);
+    const randomCollectionKey = collectionKeys[Math.floor(Math.random() * collectionKeys.length)];
+
+    // 从选中的集合中随机选择一条
+    const selectedCollection = quotesCollection[randomCollectionKey];
+    const randomIndex = Math.floor(Math.random() * selectedCollection.length);
+
+    return selectedCollection[randomIndex];
 }
